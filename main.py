d25af53ffb8b5936424b178621e67fca66333caa
dpdesenvolvedor <dpdesenvolvedor@gmail.com>
2025-09-13 20:25:19 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = 'Meu Programa'
+    ft.Text()
+
+    titulo1 = ft.Text('Cadastro Paciente')
+    titulo2 = ft.text(value = 'Cadastrode Paciente'),
+
+
+
